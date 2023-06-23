39be322dcf008f4d1b01721430c803b02bfd395c
ASWINBABUKV <aswinbabukv1@gmail.com>
2023-06-24 02:03:46 +0530

    modifications done

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var typed = new typed(".text", {
+  strings: ["Web Developer", "Problem Solver", "Student"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
